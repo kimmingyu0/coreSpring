129d21bedd24af387a77fd634a3872b1037df678
kimmingyu0 <minku4820@gmail.com>
2023-08-16 00:36:49 +0900

    Configuration 어노테이션

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -30,4 +30,19 @@ public class ConfigurationSingletonTest {
         assertEquals(memberService.getMemberRepository(), memberRepository);
         assertEquals(orderService.getMemberRepository(), memberRepository);
     }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+
+        // bean = class hello.core.AppConfig$$EnhancerBySpringCGLIB$$68989ba6
+        // @Configuration 어노테이션이 붙은 상황이면...
+        // 스프링이 CGLIB 이라는 바이트 코드 관련 라이브러리를 이용하여 AppConfig 클래스를 상속받은
+        // 임의의 다른 클래스를 만들고, 그 다른 클래스를 스프링 빈으로 등록
+        // @Configuration 어노테이션이 없이 @Bean 어노테이션만 사용하면
+        // CGLIB 기술 없이 순수한 AppConfig로 스프링 빈에 등록되지만 싱글톤은 보장되지 않는다.
+    }
 }
