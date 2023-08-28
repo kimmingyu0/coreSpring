b8604661f91c3e2868cb8b25515a457f4c2208e5
kimmingyu0 <minku4820@gmail.com>
2023-08-28 23:57:46 +0900

    InitializingBean, DisposableBean 인터페이스

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
 /**
  * 데이터 베이스 커넥션풀이나, 네트워크 소켓처럼 애플리케이션 시작 시점에 필요한 연결을 미리 해두고,
  * 애플리케이션 종료 시점에 연결을 모두 종료하는 작업을 진행하려면, 객체의 초기화와 종료 작업이 필요하다.
@@ -28,13 +31,16 @@ package hello.core.lifecycle;
  * 유지보수 관점에서 좋다.
  *
  * */
-public class NetworkClient {
+/**
+ * 초기화,소멸 (InitializingBean, DisposableBean) 인터페이스의 단점
+ * 스프링 전용 인터페이스다. 해당 코드가 스프링 전용 인터페이스에 의존한다.
+ * 외부 라이브러리에 적용할 수 없다.
+ * */
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -53,5 +59,18 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
